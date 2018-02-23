db270918c119fd04ca7cd70541bc71359cb18ffe
sanjaykrkundu <sanjaykr.kundu@gmail.com>
2018-02-23 17:57:17 +0530

    Update growableArray.c

--- growableArray.c
+++ growableArray.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* pointer = NULL;
-int size=4;
-
-void readInput(){
-    int i;
-    char ch;
-    
-    pointer = (char*)malloc(sizeof(char)*size);
-    
-	printf("Enter a sentance : \n");
-	
-	for(i = 0; i <size; i++) {
-	    
-	    if(scanf("%c",&ch)== -1)
-            break;
-        
-        pointer[i]=ch;
-        
-        if(i==size-2){
-            size*=2;
-            pointer = (char*)realloc(pointer,size);
-        }
-        printf("%d : %c %s %d\n", i,ch,pointer,size);
-	}
-	
+void assignDoubleMemory(char **pointer,int *size){
+    *size = (*size) * 2;
+    *pointer =(char*)realloc(*pointer,*size);
 }
 
+char*  readInput(int *size){
+    int i;
+    char ch;
+    char* pointer = (char*)malloc(sizeof(char)*(*size));
+	
+	for(i = 0; i <*size; i++) {
+	    if(scanf("%c",&ch)== -1)
+            break;
 
+        pointer[i]=ch;
+
+        if(i==(*size)-2){
+            assignDoubleMemory(&pointer, size);
+        }
+        //printf("%d : %c %s %d\n", i,ch,pointer,*size);
+	}
+	return pointer;
+}
 
 int main(void) {
-    readInput();
-    printf("\nsantance :  %s\n",pointer);
+    int size = 4;
+    char * pointer = readInput(&size);
+    
+    printf("\nsentance :  %s\n %d",pointer,size);
     
 	return 0;
 }
-
